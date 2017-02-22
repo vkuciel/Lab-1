d64692cd0c31be67b53a348245b9865fe224bbc4
victo_000 <victo_000@Victor>
2017-02-22 11:33:06 -0500

    hi

---
+++ src/FinanceLib.java
@@ -0,0 +1,19 @@
+
+public class FinanceLib {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static double pmt(double r, double n, double p, double f, boolean t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double pv(double r, double n, double y, double f, boolean t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/oi/ss/formula/functions/FinanceLib.java
@@ -0,0 +1,15 @@
+package oi.ss.formula.functions;
+
+public class FinanceLib {
+
+	public static double pv(double r, double n, double y, double f, boolean t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double pmt(double r, double n, double p, double f, boolean t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
